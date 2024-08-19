9d3719e53bf0c458da45de52cbc48c4d8919b7c0
matscorse <155652843+matscorse@users.noreply.github.com>
2024-08-19 11:41:35 +0100

    fixed changes to package discovery

--- setup.py
+++ setup.py
@@ -11,12 +11,13 @@ setuptools.setup(
   description='simple action pipeline python package',
   author='matscorse',
   author_email='matsco@bas.ac.uk',
-  packages=setuptools.find_packages(),
-  install_requires=['jug', 'pytest'],
+  package_dir={"": "src"},
+  packages=setuptools.find_packages(where='src'),
+  install_requires=['jug', 'pytest', 'pyyaml'],
   long_description=get_content("README.md"),
   long_description_content_type="text/markdown",
   url="https://www.github.com/antarctica",
-  classifiers=[el.lstrip() for el in """Development Status :: 3 - Alpha
+  classifiers=[el.lstrip() for el in """Development Status :: 0 - Alpha
     Intended Audience :: Science/Research
     Intended Audience :: System Administrators
     License :: OSI Approved :: MIT License
