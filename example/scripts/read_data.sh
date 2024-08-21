8790d6283c4850f134bad55b6403ef8588f779ee
matscorse <matsco@bas.ac.uk>
2024-08-21 16:35:32 +0100

    working on a better example to do with cats

--- example/scripts/read_data.sh
+++ example/scripts/read_data.sh
@@ -2,10 +2,29 @@
 
 set -e
 
-pipeline_directory = ${PIPELINE_DIRECTORY}
+pipeline_directory=$PIPELINE_DIRECTORY
+data_source="../assets/data/cat_sleepytime_duration_data.csv"
 
 echo "Reading Cat SleepyTime Duration data"
+cp ${pipeline_directory}/${data_source} ${pipeline_directory}/data_file.csv
 
+echo "Extracting columns"
+# extract column numbers for 'gender' and 'sleep_duration_minutes'
+IFS=',' read -r -a array <<< "$(head -n 1 data_file.csv)"
+for index in "${!array[@]}"
+do
+    if   [ ${array[index]} = 'gender' ]; then
+        gender=$((index+1))
 
+    elif [ ${array[index]} = 'sleep_duration_minutes' ]; then
+        duration=$((index+1))
 
+    fi
+done
 
+echo "Writing extracted columns to columns.csv"
+# write the columns we want to file
+cat data_file.csv | cut -d',' -f$gender,$duration >> ${pipeline_directory}/columns.csv
+
+echo "Removing original file"
+rm ${pipeline_directory}/data_file.csv
--- example/scripts/transform_data.sh
+++ example/scripts/transform_data.sh
@@ -2,4 +2,35 @@
 
 set -e
 
-echo "transform!!!!!!"
+pipeline_directory=$PIPELINE_DIRECTORY
+data_source="columns.csv"
+
+echo "Converting SleepyTime minutes to seconds"
+# where column == sleep_duration_minutes, convert to seconds
+IFS=',' read -r -a array <<< "$(head -n 1 ${pipeline_directory}/$data_source)"
+for index in "${!array[@]}"
+do
+    if   [ ${array[index]} = 'gender' ]; then
+        gender=$((index+1))
+        echo -n 'gender,' >> ${pipeline_directory}/seconds.csv
+
+    elif [ ${array[index]} = 'sleep_duration_minutes' ]; then
+        duration=$((index+1))
+        echo -n 'sleep_duration_seconds,' >> ${pipeline_directory}/seconds.csv
+
+    fi
+done
+echo '' >> ${pipeline_directory}/seconds.csv
+
+echo "Writing converted columns to seconds.csv"
+# write the columns we want to file
+cat columns.csv | tail -n +2 | while read line 
+do
+    value_gender=$(echo $line | cut -d',' -f$gender)
+    value_duration=$(echo $line | cut -d',' -f$duration)
+    value_duration=$(echo $value_duration " * 60" | bc )
+    echo $value_gender,$value_duration >> seconds.csv
+done
+
+echo "Removing columns file"
+rm ${pipeline_directory}/columns.csv
