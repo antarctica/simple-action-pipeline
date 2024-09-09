71785e6a2125523ce7d5ced167b1651e28536a73
matscorse <matsco@bas.ac.uk>
2024-09-09 14:02:19 +0100

    typing correction and extra helpful comment

--- src/sap/setup_logging.py
+++ src/sap/setup_logging.py
@@ -1,4 +1,6 @@
-# setup_logger.py
+# setup_logging.py
+# Import in all required scripts to get consistent logger behaviour
+
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('pipeline')
